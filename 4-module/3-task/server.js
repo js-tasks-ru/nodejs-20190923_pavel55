dcee36d88f062ac456df2d5d4dbd3dfd33fea96c
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-14 06:12:46 +0300

    add 3 files

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,36 @@
+
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
-
 server.on('request', (req, res) => {
+    
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filePath = path.join(__dirname, 'files', pathname);
+  
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+  }
 
   switch (req.method) {
     case 'DELETE':
-
+      if( !filePath ) {
+        res.statusCode = 404;
+        res.end('file not found')
+      } else {
+        fs.unlink(filePath, (error) => {
+            if(error){
+                res.statusCode = 404;
+                res.end('file not found');
+            } else {
+                res.statusCode = 200;
+                res.end('success');
+            }
+        });
+      }
       break;
 
     default:
