a1a229c3a880de861db3022f7366942016354eda
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-14 06:29:32 +0300

    ref 2 task 4 module

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -7,8 +7,8 @@ const writeFile = require('./writeFile');
 const server = new http.Server();
 server.on('request', (req, res) => {
 
-    const pathName = url.parse(req.url).pathname.slice(1);
-    const filePath = path.join(__dirname, 'files', pathName);
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filePath = path.join(__dirname, 'files', pathname);
     
     const urlArr = req.url.split('/');
     
@@ -26,7 +26,6 @@ server.on('request', (req, res) => {
             writeFile(filePath, req, res);
             break;
         default:
-            //fs.unlink(filepath, ()=>{});
             res.statusCode = 501;
             res.end('Not implemented');
     }
--- 4-module/2-task/writeFile.js
+++ 4-module/2-task/writeFile.js
@@ -3,39 +3,41 @@ const LimitSizeStream = require('./LimitSizeStream');
 
 module.exports = function writeFile( filepath, req, res ){
 
-    const maxSize = 10485760;
-    const maxStream = new LimitSizeStream({limit : maxSize});
+    const maxStream = new LimitSizeStream({limit : 1e6});
     const saveStream = fs.createWriteStream(filepath, {flags: 'wx'});
 
     req
         .pipe(maxStream)
         .pipe(saveStream);
 
-    maxStream.on('error', err=>{
-        if (err) {
+    
+    maxStream.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
             res.statusCode = 413;
-            res.end('more than max limit');
-            return;
-        } 
-    });
-
-    saveStream.on('error', (err, data) => {
-        if (err.code === 'EEXIST') {
-          res.statusCode = 409;
-          res.end('File already exists');
+            res.end('File is too big');
         } else {
             res.statusCode = 500;
-            //fs.unlink(filepath, ()=>{});
-            res.end(`Unknown error : ${err}`);
+            res.end('Internal server error');
         }
-    }).on('close', (err, data) => {
-        if(err){
-            fs.unlink(filepath, ()=>{});
-            res.end('success');
-            return;
+        fs.unlink(filepath, (err) => {});
+    });
+
+    saveStream.on('error', (error) => {
+        if (error.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('File exists');
         } else {
-            res.statusCode = 201;
-            res.end('success');
+            res.statusCode = 500;
+            res.end('Internal server error');
+            fs.unlink(filepath, (error) => {});
         }
+    }).on('close', () => {
+        res.statusCode = 201;
+        res.end('file has been saved');
+    });
+
+    res.on('close', () => {
+        if (res.finished) return;
+        fs.unlink(filepath, (error) => {});
     });
 }
