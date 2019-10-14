419483e0b569a5654554507293377b3d42e91b21
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-10-14 14:11:22 +0300

    Update app.js

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -6,11 +6,31 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let users = [];
 
 router.get('/subscribe', async (ctx, next) => {
+
+    const user = await new Promise( resolve => {        
+        users = users.concat({id: ctx.request.query.r, resolve: resolve});
+        ctx.res.on('close', () => {
+            users = users.filter(user => user.id !== ctx.request.query.r );
+        });
+    })
+    ctx.body = user;
 });
 
 router.post('/publish', async (ctx, next) => {
+    
+    const {message} = ctx.request.body;
+    
+    if (!message) {
+        ctx.status = 400;
+        ctx.body = '';
+    } else {
+        ctx.status = 200;
+        users.forEach( user => user.resolve(message) );
+        users = [];
+    }
 });
 
 app.use(router.routes());
