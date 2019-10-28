f1cbe261aef8bd323b67c5560e7d7399e984caf6
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-28 06:45:50 +0300

    change

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -2,8 +2,30 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    {
+      session: false,
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async function(email, password, done) {
+      let user;
+
+      try {
+        user = await User.findOne({email: email});
+      } catch (err) {
+        done(err);
+      };
+
+      if (!user) {
+        done(null, false, 'Нет такого пользователя');
+        return;
+      }
+
+      if (!await user.checkPassword(password)) {
+        done(null, false, 'Неверный пароль');
+        return;
+      }
+
+      done(null, user);
     }
 );
